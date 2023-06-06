671f55322d2e946a9e767ee8c3a5153d795718d3
vladyalos <vlad@yalos-solutions.com>
2023-06-06 14:57:23 +0300

    update the repozitory

---
+++ project folder/file.php
@@ -0,0 +1,16 @@
+<?php
+
+echo("hello");
+
+public function writeMessage(string $string):void
+{
+        ehco ("hello there $string");        
+    }
+
+writeMessage("string");
+
+class Messages 
+{
+    public function writeParameterMessage(){
+            }
+}
